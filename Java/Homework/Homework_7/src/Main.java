85c2466d82715c89a093648a8bdcb872beea69f2
Grisha <grishalikhtarovich@gmail.com>
2023-01-10 08:54:28 +0100

    Homework_7

--- Java/Homework/Homework_7/src/Main.java
+++ Java/Homework/Homework_7/src/Main.java
@@ -6,6 +6,10 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
 
+    static StringBuilder stringBuilder = new StringBuilder();
+    static StringBuffer stringBuffer = new StringBuffer();
+
+
     public static void main(String[] args) {
 
 //        int a = scanner.nextInt();
@@ -57,6 +61,52 @@ public class Main {
         System.out.println("Ploschad priamoygolnica: " + Area.rectangle(x,y));
         System.out.println("Ploschad treygolnika: " + Area.triangle(x,y));
         System.out.println("Ploschad kryga: " + Area.circle(x));
+
+
+
+
+
+
+        stringBuffer.append("Privet, StringBuffer");
+        System.out.println(stringBuffer);
+
+        String numbers = "0123456789";
+
+        StringBuffer sb = new StringBuffer(numbers);
+
+        System.out.println(sb.substring(6));
+        System.out.println(sb.substring(4, 8));
+        System.out.println(sb.replace(3, 5, "ABCDE"));
+
+        sb = new StringBuffer(numbers);
+        System.out.println(sb.reverse());
+        sb.reverse(); // Вернем изначальный порядок
+
+        sb = new StringBuffer(numbers);
+        System.out.println(sb.delete(5, 9));
+        System.out.println(sb.deleteCharAt(1));
+        System.out.println(sb.insert(1, "One"));
+
+        System.out.println();
+
+        stringBuilder.append("Privet, StringBilder");
+        System.out.println(stringBuilder);
+
+        String number = "0123456789";
+        StringBuilder s = new StringBuilder(number);
+
+        System.out.println(s.substring(3)); //3456789
+        System.out.println(s.substring(4, 8)); //4567
+        System.out.println(s.replace(3, 5, "ABCDE")); //012ABCDE56789
+
+        s = new StringBuilder(number);
+        System.out.println(s.reverse()); //9876543210
+        s.reverse(); // Вернем изначальный порядок
+
+        s = new StringBuilder(number);
+        System.out.println(s.delete(5, 9)); //012349
+        System.out.println(s.deleteCharAt(1)); //02349
+        System.out.println(s.insert(1, "One")); //0One2349
 
 
     }
